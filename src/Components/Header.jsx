0115cece11e4455b0ee7eeb8791689556b45a7c5
aysglgnr <124514563+aysglgnr@users.noreply.github.com>
2024-11-12 22:06:26 +0300

    son2

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
 <div className='name'>
     <h2>BİRLİKTE</h2>
     <h1><span>GERİ</span> DÖNÜŞTÜRELİM</h1>
-    <p className='details'></p>
+    <p className='details'>Atıkları Değere Dönüştür, Geleceği Şekillendir!</p>
 </div>
 
     </div>
